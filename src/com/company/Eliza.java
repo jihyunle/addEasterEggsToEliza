4b4500be1cafc08498dcfb043a3aa65e6c24c98c
jihyunle <jennifer.jlee@outlook.com>
2019-01-29 10:49:48 -0500

    Needs an option to loop while pig mode is on

--- src/com/company/Eliza.java
+++ src/com/company/Eliza.java
@@ -7,45 +7,96 @@ import java.util.Scanner;
 public class Eliza {
     public static void main(String[]args){
 
+        String userInput = " ";
+        boolean userQuits = false;
+        String elizaSays = " ";
+        int pigCount = 0;
+
         Scanner input = new Scanner(System.in);
         System.out.println("Good day. How are you doing today?");
 
-        String userInput = " ";
-        boolean userQuit = false;
-        String elizaSays = " ";
-
-        while(!userQuit){
+        while(!userQuits){
             System.out.println("Enter your response here, or press Q to quit.");
             userInput = input.nextLine();
 
-            // Check if user wants to quit
-            if (userQuit = checkQuitCommand(userInput)){
+            // if user types a quit command
+            if (userQuits = checkQuitCommand(userInput)){
                 break;
-            }
-
-
-            System.out.println();
-
-            // After user input is read, generate a random number between 0 and 1
-            Random rnd = new Random();
-            int responseOption = rnd.nextInt(2);
-            if(responseOption==1){
-                // call method that randomly chooses hedge
-                elizaSays = respondWithHedges();
-
+            // if user types "caps"
+            }else if (userInput.equalsIgnoreCase("caps")){
+                System.out.println("Enter your response here, or press Q to quit.");
+                userInput = input.nextLine();
+                elizaSays = allCaps(userInput);
+            // if user types "pig"
+            }else if (userInput.equalsIgnoreCase("pig")){
+                    System.out.println("Enter your response here, or press Q to quit.");
+                    userInput = input.nextLine();
+                    while (!userInput.equalsIgnoreCase("pig")){
+                        String str = replaceWords(userInput);
+                        elizaSays = pigLatin(str);
+                    }
             }else{
-                // call method that first replaces words in user input
-                // then prepends the qualifier
-                elizaSays = respondWithQualifiers(userInput);
+                elizaSays = hedgeOrQualifier(userInput);
             }
 
             System.out.println(elizaSays);
-            System.out.println();
+
         }
 
         System.out.println(">>> END");
         input.close();
         System.exit(0);
+    }
+
+    public static String hedgeOrQualifier(String userInput){
+        String elizaSays = "";
+        // After user input is read, generate a random number between 0 and 1
+        Random rnd = new Random();
+        int responseOption = rnd.nextInt(2);
+        if(responseOption==1){
+            // call method that randomly chooses hedge
+            return elizaSays = respondWithHedges();
+
+        }else{
+            // call method that first replaces words in user input
+            // then prepends the qualifier
+            return elizaSays = respondWithQualifiers(userInput);
+        }
+    }
+
+    public static String allCaps(String str){
+        String newStr = str.toUpperCase();
+        return newStr;
+    }
+
+    public static String pigLatin(String str){
+
+        String newStr = "";
+        char firstletter = str.toLowerCase().charAt(0);
+        char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+
+        // eliza speaks in pig latin
+        for(int i=0; i<vowels.length; i++){
+            // if first letter vowel, append "way" or "tay" randomly chosen
+            if (firstletter == vowels[i]){
+                int num = (int)Math.random()*2;
+                if (num==0){
+                    // append "way"
+                    newStr = str + "way";
+                }else{
+                    // append "tay"
+                    newStr = str + "tay";
+                }
+            }
+            // else if first letter consonant, append "ay"
+            else {
+                newStr = str + "ay";
+
+            }
+
+        }
+        return newStr;
+
     }
 
     public static String respondWithHedges(){
@@ -74,7 +125,6 @@ public class Eliza {
     }
 
     public static String replaceWords(String str){
-
         String[] words = str.split(" ");
         String newStr = "";
 
@@ -107,8 +157,9 @@ public class Eliza {
     }
 
     public static boolean checkQuitCommand(String str){
-
+        // initialize default bool as false
         boolean quit = false;
+        // bool changes to true if quit commands are typed
         if (str.equalsIgnoreCase("Q") || str.equalsIgnoreCase("i am feeling great")) {
             quit = true;
         }
